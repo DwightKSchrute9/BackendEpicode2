41ce222ae986fb349f9634f1bd960e5ff6007fbb
DwightKSchrute9 <silviolampignano@gmail.com>
2023-03-24 17:15:47 +0100

    EPIC-ENERGY-SERVICES_BuildWeek

--- BuildWeek_EPIC ENERGY SERVICES/EPIC-ENERGY-SERVICES/src/main/java/com/example/EPIC/ENERGY/SERVICES/DataLoaderCSV/CsvImporter.java
+++ BuildWeek_EPIC ENERGY SERVICES/EPIC-ENERGY-SERVICES/src/main/java/com/example/EPIC/ENERGY/SERVICES/DataLoaderCSV/CsvImporter.java
@@ -2,7 +2,6 @@ package com.example.EPIC.ENERGY.SERVICES.DataLoaderCSV;
 
 
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -16,9 +15,10 @@ public class CsvImporter {
   public static void importCsvData() throws IOException {
     String filePath = "/path/to/csv/file.csv";
     Reader reader = Files.newBufferedReader(Paths.get(filePath));
-    CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
-    for (CSVRecord csvRecord : csvParser) {
-      // Process CSV record
-    }
+    try (CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT)) {
+		for (CSVRecord csvRecord : csvParser) {
+		  // Process CSV record
+		}
+	}
   }
 }
